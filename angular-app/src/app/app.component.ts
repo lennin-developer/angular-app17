e453e073a784f5f252c1f1db0a605cfb601418f3
lennin-developer <lennin.developer@gmail.com>
2024-06-11 21:59:46 -0400

    passing data to parent from child using @Output

--- angular-app/src/app/app.component.ts
+++ angular-app/src/app/app.component.ts
@@ -1,4 +1,4 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { RouterOutlet } from '@angular/router';
 import { CounterComponent } from './counter/counter.component';
@@ -10,15 +10,31 @@ import { CounterComponent } from './counter/counter.component';
   templateUrl: './app.component.html',
   styleUrl: './app.component.css',
 })
-export class AppComponent {
+export class AppComponent implements OnInit {
   title: string = 'Hola Mundo Angular';
   users: string[] = ['Mishel', 'Lennin', 'Pilar'];
   subtitle: string = 'Contador con estado de sesion';
 
   visible: boolean = false;
+  counter: number = 0;
+
+  ngOnInit(): void {
+    //localStorage es persistente, sessionStorage no, es decir localStorage se mantiene aunque cerremos el navegador, sessionStorage no
+    //this.counter = parseInt(sessionStorage.getItem('counter')!) || 0;
+    this.counter =
+      sessionStorage.getItem('counter') != undefined
+        ? parseInt(sessionStorage.getItem('counter')!)
+        : 0;
+    console.log('Creando componente AppComponent');
+  }
 
   setVisible(): void {
     this.visible = this.visible ? false : true;
     console.log('Hemos hecho click en el setVisible');
+  }
+
+  setCounter(counter: number): void {
+    this.counter = counter;
+    console.log('Hemos hecho click en el setCounter');
   }
 }
--- angular-app/src/app/counter/counter.component.ts
+++ angular-app/src/app/counter/counter.component.ts
@@ -1,20 +1,24 @@
-import { Component, Input, OnInit } from '@angular/core';
+import { Component, Input, EventEmitter, OnInit, Output } from '@angular/core';
 
 @Component({
   selector: 'app-counter',
   standalone: true,
   imports: [],
-  templateUrl: './counter.component.html'
+  templateUrl: './counter.component.html',
 })
 export class CounterComponent implements OnInit {
+  counter: number = 0;
+  @Input() title!: string;
 
-  counter:number = 0;
-  @Input() title!:string;
+  @Output() counterEmmit: EventEmitter<number> = new EventEmitter();
 
   ngOnInit(): void {
     //localStorage es persistente, sessionStorage no, es decir localStorage se mantiene aunque cerremos el navegador, sessionStorage no
     //this.counter = parseInt(sessionStorage.getItem('counter')!) || 0;
-    this.counter = sessionStorage.getItem('counter') != undefined ? parseInt(sessionStorage.getItem('counter')!) : 0;
+    this.counter =
+      sessionStorage.getItem('counter') != undefined
+        ? parseInt(sessionStorage.getItem('counter')!)
+        : 0;
     console.log('Creando componente CounterComponent');
   }
 
@@ -22,5 +26,6 @@ export class CounterComponent implements OnInit {
     this.counter = this.counter + 1;
     sessionStorage.setItem('counter', this.counter.toString());
     //localStorage.setItem('counter', this.counter.toString());
+    this.counterEmmit.emit(this.counter);
   }
 }
