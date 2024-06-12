b902decb1e9ce451cee179b80fccef85a0a8140f
lennin-developer <lennin.developer@gmail.com>
2024-06-11 21:33:45 -0400

    Storing the counter value in the local storage or session storage

--- angular-app/src/app/counter/counter.component.ts
+++ angular-app/src/app/counter/counter.component.ts
@@ -1,4 +1,4 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
 
 @Component({
   selector: 'app-counter',
@@ -6,10 +6,20 @@ import { Component } from '@angular/core';
   imports: [],
   templateUrl: './counter.component.html'
 })
-export class CounterComponent {
+export class CounterComponent implements OnInit {
+
   counter:number = 0;
+
+  ngOnInit(): void {
+    //localStorage es persistente, sessionStorage no, es decir localStorage se mantiene aunque cerremos el navegador, sessionStorage no
+    //this.counter = parseInt(sessionStorage.getItem('counter')!) || 0;
+    this.counter = sessionStorage.getItem('counter') != undefined ? parseInt(sessionStorage.getItem('counter')!) : 0;
+    console.log('Creando componente CounterComponent');
+  }
 
   setCounter(): void {
     this.counter = this.counter + 1;
+    sessionStorage.setItem('counter', this.counter.toString());
+    //localStorage.setItem('counter', this.counter.toString());
   }
 }
