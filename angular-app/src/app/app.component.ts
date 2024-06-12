2d74fd35e67c5f7b5c33027153489828af106dc5
lennin-developer <lennin.developer@gmail.com>
2024-06-11 21:44:58 -0400

    Passing data from parent to child compoment using @Input

--- angular-app/src/app/app.component.ts
+++ angular-app/src/app/app.component.ts
@@ -6,13 +6,14 @@ import { CounterComponent } from './counter/counter.component';
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [CounterComponent,RouterOutlet], //CommonModule es requerido cuando en nuestro html usamos las direvtivas ngIf, ngFor, etc
+  imports: [CounterComponent, RouterOutlet], //CommonModule es requerido cuando en nuestro html usamos las direvtivas ngIf, ngFor, etc
   templateUrl: './app.component.html',
   styleUrl: './app.component.css',
 })
 export class AppComponent {
   title: string = 'Hola Mundo Angular';
   users: string[] = ['Mishel', 'Lennin', 'Pilar'];
+  subtitle: string = 'Contador con estado de sesion';
 
   visible: boolean = false;
 
--- angular-app/src/app/counter/counter.component.ts
+++ angular-app/src/app/counter/counter.component.ts
@@ -1,4 +1,4 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, Input, OnInit } from '@angular/core';
 
 @Component({
   selector: 'app-counter',
@@ -9,6 +9,7 @@ import { Component, OnInit } from '@angular/core';
 export class CounterComponent implements OnInit {
 
   counter:number = 0;
+  @Input() title!:string;
 
   ngOnInit(): void {
     //localStorage es persistente, sessionStorage no, es decir localStorage se mantiene aunque cerremos el navegador, sessionStorage no
